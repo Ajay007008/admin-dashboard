a8f6b5327f5d94db3c80b81652330641837c0166
Ajay Singh <ajay@Ajays-MacBook-Air.local>
2025-04-04 23:22:12 +0530

    “final”

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New"
 
+import "../src/style/dark.scss";
+
+
+import { productInputs, userInputs } from "./formSource.js";
+
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext.js";
 
 function App() {
+
+const {darkMode} = useContext(DarkModeContext)
   return (
-    <div className="App"> 
+    <div className={darkMode ? "app dark" : "app"}> 
          <BrowserRouter>
         <Routes>
             <Route path="/">
-                <Route index element={<Home />} />
+                <Route index element={<Home/>} />
                 <Route path="login" element={<Login/>}/>
                 
 
@@ -22,14 +31,14 @@ function App() {
                 <Route path="users">
                     <Route index element={<List/>}/>
                     <Route path=":userId" element={<Single/>}/>
-                    <Route path="new" element={<New/>}/>
+                    <Route path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
                 </Route>
   
                   {/* Products Routes */}
                 <Route path="products">
                     <Route index element={<List/>}/>
                     <Route path=":productId" element={<Single/>}/>
-                    <Route path="new" element={<New/>}/>
+                    <Route path="new" element={<New inputs= {productInputs} title="Add New Product"/>}/>
                 </Route>
 
 
